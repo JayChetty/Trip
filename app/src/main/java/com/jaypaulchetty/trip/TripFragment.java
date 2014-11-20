30bd282cfb632fa53122668a8bc11e56bae0ba29
JayChetty <jaypaulchetty@gmail.com>
2014-11-20 20:58:15 +0000

    have edit text buttons for non start and end country, have button to check route

--- app/src/main/java/com/jaypaulchetty/trip/TripFragment.java
+++ app/src/main/java/com/jaypaulchetty/trip/TripFragment.java
@@ -1,15 +1,21 @@
 package com.jaypaulchetty.trip;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 
 public class TripFragment extends ListFragment {
@@ -19,6 +25,7 @@ public class TripFragment extends ListFragment {
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
         Log.d("TripFragement", "Starting the Fragment YO");
         CountryJSONLoader loader = new CountryJSONLoader(getActivity());
         try {
@@ -29,14 +36,39 @@ public class TripFragment extends ListFragment {
         }
         mTrip = new TripCreator(mCountries).createTrip();
 
-        ArrayAdapter<Country> adapter = new ArrayAdapter<Country>(getActivity(),android.R.layout.simple_list_item_1,mTrip.getRoute(0));
+        TripArrayAdapter adapter = new TripArrayAdapter(getActivity(),android.R.layout.simple_list_item_1,mTrip.getRoute(0));
         setListAdapter(adapter);
 
     }
 
-//    public View onCreateView(LayoutInflater inflater, ViewGroup parent, Bundle savedInstanceState){
-//        View v = inflater.inflate(R.layout.fragment_trip, parent, false);
-//        return v;
-//    }
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater){
+       super.onCreateOptionsMenu(menu,inflater);
+       inflater.inflate(R.menu.trip,menu);
+    }
+
+
+    public class TripArrayAdapter extends ArrayAdapter<Country>{
+
+        public TripArrayAdapter(Context context, int resource, List<Country> objects) {
+            super(context, resource, objects);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            Log.d("GettingView","position" + position);
+            View out;
+            if(position == getCount()-1 || position == 0 ) {
+                out = super.getView(position, convertView, parent);
+            }
+            else {
+                EditText view = new EditText(getContext());
+                out = view;
+                view = null;
+            }
+            return out;
+
+        }
+    }
 
 }
