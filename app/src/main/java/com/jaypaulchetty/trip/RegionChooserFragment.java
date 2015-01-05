dbf769d684c7ffbe4e37f8084c712933b0c39514
JayChetty <jaypaulchetty@gmail.com>
2015-01-05 21:56:50 +0000

    manual list item for regions

--- app/src/main/java/com/jaypaulchetty/trip/RegionChooserFragment.java
+++ app/src/main/java/com/jaypaulchetty/trip/RegionChooserFragment.java
@@ -5,10 +5,13 @@ import android.os.Bundle;
 import android.support.v4.app.ListFragment;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,7 +27,9 @@ public class RegionChooserFragment extends ListFragment {
         setHasOptionsMenu(true);
         TripCreator.setCountries(getActivity());
         mRegionScores = RegionScores.get(getActivity());
-        ArrayAdapter adapter = new ArrayAdapter(getActivity(),android.R.layout.simple_list_item_1, RegionScores.getRegions());
+        ArrayList regionList = new ArrayList(Arrays.asList(RegionScores.getRegions()));
+        RegionAdapter adapter= new RegionAdapter(regionList);
+//        ArrayAdapter adapter = new ArrayAdapter(getActivity(),android.R.layout.simple_list_item_1, RegionScores.getRegions());
         setListAdapter(adapter);
     }
 
@@ -37,5 +42,23 @@ public class RegionChooserFragment extends ListFragment {
         i.putExtra(REGION_FOR_TRIPS, region);
 //        i.putExtra(RegionFragment.REGION_BEST_TIME, mRegionTimes.getTime(region));
         startActivity(i);
+    }
+
+    private class RegionAdapter extends ArrayAdapter<String>{
+        public RegionAdapter(ArrayList<String> regions){
+            super(getActivity(),0, regions);
+        }
+        public View getView(int position, View convertView, ViewGroup parent){
+            if(convertView == null){
+                convertView = getActivity().getLayoutInflater().inflate(R.layout.list_item_region,null);
+            }
+
+            String regionName = getItem(position);
+            TextView textview = (TextView) convertView.findViewById(R.id.region_list_item_title);
+            textview.setText(regionName);
+
+            return convertView;
+        }
+
     }
 }
